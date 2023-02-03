c3d88c2b2eb2b9abbba036990685f4c598bc6dfc
A. Osama <Altimos@desktop.com>
2023-02-04 01:15:45 +0200

    Minor change to consts

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header("Access-Control-Allow-Headers: *");
 
-define("MODEL", "models" . DIRECTORY_SEPARATOR);
-define("DATA", "data" . DIRECTORY_SEPARATOR);
-define("CONTROLLER", "controllers" . DIRECTORY_SEPARATOR);
-define("INC", "inc" . DIRECTORY_SEPARATOR);
+define("MODEL", "Models" . DIRECTORY_SEPARATOR);
+define("DATA", "Data" . DIRECTORY_SEPARATOR);
+define("CONTROLLER", "Controllers" . DIRECTORY_SEPARATOR);
+define("INC", "Inc" . DIRECTORY_SEPARATOR);
 
 
 // Autoloading
